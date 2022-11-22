1a04322a49a59573e920e70563aaf0c0d792c5d9
Hanif Bahari <hanif.bahari@gmail.com>
2022-11-22 14:15:02 +0100

    figmagic chakra

---
+++ src/design-system/chakra/tokenIntegration.ts
@@ -0,0 +1,4 @@
+export { default as colors } from '../figmagic/tokens/colors';
+export { default as fontFamilies } from '../figmagic/tokens/fontFamilies';
+export { default as fontSizes } from '../figmagic/tokens/fontSizes';
+export { default as fontWeights } from '../figmagic/tokens/fontWeights';
